34c24fff058375f428f40c1f01e25de0786dacc9
Sahil Dahat <sahil.developerinfo@gmail.com>
2023-12-10 17:40:16 +0530

    folder setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button"
+//https://nextjs.org/docs/app/building-your-application/routing/route-groups
+
+export default function DashboardPage() {
+  return (
+    <Button variant="secondary">Click Me!</Button>
+  )
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,7 @@
+const LandingPage = () => {
+  return (
+    <div>LandingPage (unprotected) </div>
+  )
+}
+
+export default LandingPage;
