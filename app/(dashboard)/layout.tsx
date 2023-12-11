a5d3580c82e47e780677f57b57ec346c7c2add7c
Sahil Dahat <sahil.developerinfo@gmail.com>
2023-12-11 11:46:54 +0530

    added navbar & sidebar component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,15 @@
+import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
+
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:w-80 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-        <div className="text-white">SIDEBAR!</div>
+        <Sidebar/>
       </div>
       <main className="md:pl-80 text-black">
-        CONTENT!
+        <Navbar/>
         {children}
-        {/*<navbar/>*/}
       </main>
     </div>
   );
