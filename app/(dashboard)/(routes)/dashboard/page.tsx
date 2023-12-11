769eaaa0c477f52627ecbc2e7d2ab734cd42d124
Sahil Dahat <sahil.developerinfo@gmail.com>
2023-12-11 11:47:49 +0530

    removed UserButton & added to Navbar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,9 @@
-import { Button } from "@/components/ui/button"
-import { UserButton } from "@clerk/nextjs"
 //https://nextjs.org/docs/app/building-your-application/routing/route-groups
 
 export default function DashboardPage() {
   return (
     <div>
-      <Button>Click Me!</Button>
-      <UserButton afterSignOutUrl="/"/>
+      <p>nothing!</p>
     </div>
   )
 }
