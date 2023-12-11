288eed07adae6a3cf9df92ab9357e5bb22dfbba8
Sahil Dahat <sahil.developerinfo@gmail.com>
2023-12-11 11:23:18 +0530

    AuthLayout doubt!

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,8 @@
 const AuthLayout = ({children} : {children : React.ReactNode;}) => {
+  //how does this AuthLayout connect with the sign in & sign up page?
   return (
     <div className="flex items-center justify-center h-full">{children}</div>
   )
 }
 
-export default AuthLayout
+export default AuthLayout;
