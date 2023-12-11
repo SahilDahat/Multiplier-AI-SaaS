28d7191b417ece101d0619b98ac89a3cb6461daa
Sahil Dahat <sahil.developerinfo@gmail.com>
2023-12-11 11:21:27 +0530

    [[...sign-up]] convention usage

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,3 +1,8 @@
+//The double brackets [[...sign-up]] are used for catch-all routes, 
+//which means this route will match paths like /sign-up/anything/here. 
+//The content of the anything part will be available as a parameter, 
+//which can be accessed in your page component.
+//This allows for flexible routing and handling of different sign-up scenarios or additional parameters.
 import { SignIn } from "@clerk/nextjs";
  
 export default function Page() {
